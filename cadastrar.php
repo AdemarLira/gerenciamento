0fc0edaf91c98e1aee7902dfad3044ec235de20e
Ademar de Souza Lira Neto <ademarliraneto@gmail.com>
2024-09-11 10:58:35 -0300

    organizando as paginas de cadastro e login

--- cadastrar.php
+++ cadastrar.php
@@ -9,7 +9,7 @@ include_once('php/config.php');
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="css/styleLogin.css">
+  <link rel="stylesheet" href="css/styleCadastrar.css">
   <title>Cadastro de Usuário</title>
 </head>
 
@@ -18,7 +18,7 @@ include_once('php/config.php');
     <div class="custom-container">
       <div id="cabecalho">
         <div id="conteudo-cabecalho">
-          <h1>Login</h1>
+          <h1>Cadastro</h1>
         </div>
       </div>
       
